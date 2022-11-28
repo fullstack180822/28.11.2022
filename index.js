88064b8dd459b7efb512a8c097dbf2b8ca826006
micserv010821 <dotnet290320@gmail.com>
2022-11-28 20:12:40 +0200

    upload all

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const sqlite3 = require('sqlite3').verbose();
-const db_file_loc = './db/db2.db'
+const db_file_loc = './db/db1.db'
 
-const db = new sqlite3.Database(db_file_loc, (err) => {
+function open_db() {
+ return new sqlite3.Database(db_file_loc, (err) => {
     if (err) {
         console.log(`Failed to connect to ${db_file_loc}`);
     }
@@ -9,11 +10,43 @@ const db = new sqlite3.Database(db_file_loc, (err) => {
         console.log(`Successfully connected to ${db_file_loc}`);
     }
 })
+}
 
+function insert(db) {
+    const data = [7, 'DAN', 18, 'MEXICO', 32000]
+    const sql_insert = `INSERT INTO COMPANY (ID,NAME,AGE,ADDRESS,SALARY)
+                        VALUES (?, ?, ? ,?, ? );`
+    db.run(sql_insert, data, err => {
+        if (err) {
+            console.log(`ERROR: ${err}`);
+        }
+        else {
+            console.log(`INSERTED ${data}`);
+        }
+    })
+}
+
+function select_all(db) {
+    db.serialize(() => {
+        console.log(`SELECT * FROM COMPANY`)
+        db.each(`SELECT * FROM COMPANY`, (err, row) => {
+            if (err) {
+                console.log(`ERROR: ${err}`);
+            }
+            else {
+                console.log(row)
+            }
+        })
+    })
+}
+
+function select_over_30k(db) {
+    
 // create another query which selects only salary larger than 30,000
-
 db.serialize(() => {
-    db.each(`SELECT * FROM COMPANY`, (err, row) => {
+    console.log(`==========================================================`);
+    console.log(`SELECT * FROM COMPANY WHERE SALARY > 30,000`)
+    db.each(`SELECT * FROM COMPANY WHERE SALARY > 30000`, (err, row) => {
         if (err) {
             console.log(`ERROR: ${err}`);
         }
@@ -22,13 +55,29 @@ db.serialize(() => {
         }
     })
 })
+}
+
+function close_db(db) {
+    db.close(err => {
+        if (err) {
+            console.log(err.message);
+        }
+        else {
+            console.log('Database connection closed!');
+        }
+    })
+}
+
+const db = open_db()
+select_all(db)
+setTimeout(() => select_over_30k(db), 500)
+setTimeout(() => close_db(db), 800);
+
+//-- INSERT
+//-- UPDATE (db.run)
+//-- DELETE (db.run)
 
 
-db.close(err => {
-    if (err) {
-        console.log(err.message);
-    }
-    else {
-        console.log('Database connection closed!');
-    }
-})
+
+
+
