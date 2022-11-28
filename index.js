04cc7bdb7765b0347ac6f621a4380dac6b89de7a
micserv010821 <dotnet290320@gmail.com>
2022-11-28 17:59:11 +0200

    upload all

---
+++ index.js
@@ -0,0 +1,10 @@
+
+// npm i sqlite3
+// .gitignore -- node_modukes package-lock.json
+// github
+// script -- start, debug
+// require
+const os = require('os');
+console.log(`Free memory: ${os.freemem()}`);
+// open onodejs os module - search for this command
+// print how much memory total i have
