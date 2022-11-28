bb5b773d04df30e8076b2e1cf400de76ce2a6dbb
micserv010821 <dotnet290320@gmail.com>
2022-11-28 19:02:30 +0200

    Update index.js

---
+++ index.js
@@ -0,0 +1,32 @@
+const sqlite3 = require('sqlite3').verbose();
+const db_file_loc = './db/db2.db'
+
+const db = new sqlite3.Database(db_file_loc, (err) => {
+    if (err) {
+        console.log(`Failed to connect to ${db_file_loc}`);
+    }
+    else {
+        console.log(`Successfully connected to ${db_file_loc}`);
+    }
+})
+
+db.serialize(() => {
+    db.each(`SELECT * FROM COMPANY`, (err, row) => {
+        if (err) {
+            console.log(`ERROR: ${err}`);
+        }
+        else {
+            console.log(row)
+        }
+    })
+})
+
+
+db.close(err => {
+    if (err) {
+        console.log(err.message);
+    }
+    else {
+        console.log('Database connection closed!');
+    }
+})
